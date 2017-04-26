4b31ac7a15ab664f477f8132827a7b98986211b9
alvarollmenezes <alvarollmenezes@gmail.com>
2017-04-26 12:56:36 -0300

    Adiciona cache no proxy do buscabus

--- src/routes/buscabus.js
+++ src/routes/buscabus.js
@@ -1,11 +1,25 @@
-// const apicache = require( 'apicache' ).options( { debug: false } ).middleware;
+const apicache = require( 'apicache' ).options( { debug: false, appendKey: [ 'bodyRaw' ] } ).middleware;
 const request = require( 'request-promise' );
 const appConfig = require( '../config/app' );
 const buscaBusConfig = require( '../config/buscabus' );
 
 module.exports = app => {
 
-    app.use( '/buscabus/*', /*apicache( '30 seconds' ),*/ ( req, res, next ) => {
+    app.use( ( req, res, next ) => {
+        if ( req.body ) {
+            req.bodyRaw = JSON.stringify( req.body );
+        }
+        next();
+    } );
+
+    app.use( '/buscabus/svc/json/db/pesquisarPontosDeParada', apicache( '6 hours' ), proxyBuscabus );
+    app.use( '/buscabus/svc/json/db/listarPontosDeParada', apicache( '6 hours' ), proxyBuscabus );
+    app.use( '/buscabus/svc/json/db/pesquisarPontosDeParada', apicache( '6 hours' ), proxyBuscabus );
+    app.use( '/buscabus/svc/json/db/listarItinerarios', apicache( '6 hours' ), proxyBuscabus );
+
+    app.use( '/buscabus/*', apicache( '30 seconds' ), proxyBuscabus );
+
+    function proxyBuscabus( req, res, next ) {
         const apiPath = req.originalUrl.replace( `${appConfig.path}/buscabus/`, '' );
         const auth = 'Basic ' + new Buffer( buscaBusConfig.user + ':' + buscaBusConfig.password ).toString( 'base64' );
 
@@ -24,5 +38,6 @@ module.exports = app => {
         request( options )
             .then( data => res.json( data ) )
             .catch( err => next( err ) );
-    } );
+    }
+
 };
